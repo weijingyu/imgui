535c1559123c6f8f47d4f27a4d7ab3ef82e338ac
Wei <weijingyu@outlook.com>
2022-08-11 19:53:53 +0800

    add premake5

---
+++ premake5.lua
@@ -0,0 +1,65 @@
+project "imgui"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui_internal.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+
+        "imgui.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_opengl3_loader.h",
+
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.cpp",
+        
+    }
+
+    includedirs
+    {
+        ".",
+        "./backends",
+        "%{wks.location}/whale/external/glfw/include"
+    }
+
+    links
+    {
+        "glfw"
+    }
+
+    defines {
+        "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++20"
+        staticruntime "On"
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        cppdialect "C++20"
+        staticruntime "On"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
